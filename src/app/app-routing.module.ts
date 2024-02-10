84f98de113a71536c8234df6571ed9b36ba325be
AttilaJager <jagerattila@gmail.com>
2024-02-10 14:54:51 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HibabejelentesComponent } from './hibabejelentes/hibabejelentes.component';
+import { ErrorReportComponent } from './error-report/error-report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HibabejelentesComponent},
+  {path:"errorreport", component:ErrorReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HibabejelentesComponent } from './hibabejelentes/hibabejelentes.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import {AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import {Environments } from './environments';
+import { ErrorReportComponent } from './error-report/error-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HibabejelentesComponent,
+    NavbarComponent,
+    ErrorReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(Environments.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
